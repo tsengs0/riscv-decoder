32d0b1cc8364c766c691ad64979e4d8ded5b2140
pcotret <pascal.cotret@gmail.com>
2021-11-11 21:55:41 +0100

    Modifying decoding functions

--- riscv/riscv_func.py
+++ riscv/riscv_func.py
@@ -1,6 +1,5 @@
 import riscv.riscv_cte as rc
 
-
 def instruction_type(opcode):
     if ((opcode == 0x37)
      or (opcode == 0x17)
@@ -20,37 +19,37 @@ def instruction_type(opcode):
 
 
 def u_decoding(inst):
-    imm    = inst & rc.U_IMM_MASK
-    rd     = inst & rc.RD_MASK
+    imm    = (inst & rc.U_IMM_MASK) >> 12
+    rd     = (inst & rc.RD_MASK)    >> 7
     opcode = inst & rc.OPCODE_MASK
     return imm, rd, opcode
 
 
 def i_decoding(inst):
-    imm    = inst & rc.I_IMM_MASK
-    rs1    = inst & rc.RS1_MASK
-    funct3 = inst & rc.FUNCT3_MASK
-    rd     = inst & rc.RD_MASK
+    imm    = (inst & rc.I_IMM_MASK)  >> 20
+    rs1    = (inst & rc.RS1_MASK)    >> 15
+    funct3 = (inst & rc.FUNCT3_MASK) >> 12
+    rd     = (inst & rc.RD_MASK)     >> 7
     opcode = inst & rc.OPCODE_MASK
     return imm, rs1, funct3, rd, opcode
 
 
 def r_decoding(inst):
-    funct7 = inst & rc.FUNCT7_MASK
-    rs2    = inst & rc.RS2_MASK
-    rs1    = inst & rc.RS1_MASK
-    funct3 = inst & rc.FUNCT3_MASK
-    rd     = inst & rc.RD_MASK
+    funct7 = (inst & rc.FUNCT7_MASK) >> 25
+    rs2    = (inst & rc.RS2_MASK)    >> 20
+    rs1    = (inst & rc.RS1_MASK)    >> 15
+    funct3 = (inst & rc.FUNCT3_MASK) >> 12
+    rd     = (inst & rc.RD_MASK)     >> 7
     opcode = inst & rc.OPCODE_MASK
     return funct7, rs2, rs1, funct3, rd, opcode
 
 
 def s_decoding(inst):
-    imm11  = inst & rc.S_IMM115_MASK
-    rs2    = inst & rc.RS2_MASK
-    rs1    = inst & rc.RS1_MASK
-    funct3 = inst & rc.FUNCT3_MASK
-    imm40  = inst & rc.S_IMM40_MASK
+    imm11  = (inst & rc.S_IMM115_MASK) >> 25
+    rs2    = (inst & rc.RS2_MASK)      >> 20
+    rs1    = (inst & rc.RS1_MASK)      >> 15
+    funct3 = (inst & rc.FUNCT3_MASK)   >> 12
+    imm40  = (inst & rc.S_IMM40_MASK)  >> 7
     opcode = inst & rc.OPCODE_MASK
     return imm11, rs2, rs1, funct3, imm40, opcode
 
